b265e7dffda176caceac7ca046649f9baaa127bc
fwend <josvanuden@gmail.com>
2021-12-14 10:11:21 +0100

    refactor

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-var canvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var g = canvas.getContext("2d");
+const g = canvas.getContext("2d");
 g.fillStyle = "#6AA84F";
 
-var fern = [];
-var pixelCount = 0;
-var limit = 400000;
+const fern = [];
+const limit = 400000;
+let pixelCount = 0;
 
 function createFern(leftMargin, topMargin, w, h) {
-    var x = 0, y = 0;
+    let x = 0, y = 0;
 
-    for (var i = 0; i < limit; i++) {
-        var tmpx, tmpy;
-        var r = Math.random();
+    for (let i = 0; i < limit; i++) {
+        let tmpx, tmpy;
+        let r = Math.random();
 
         if (r <= 0.01) {
             tmpx = 0;
@@ -40,10 +40,10 @@ function createFern(leftMargin, topMargin, w, h) {
 }
 
 function animate() {
-    var requestID = requestAnimationFrame(animate);
+    let requestID = requestAnimationFrame(animate);
 
     if (pixelCount < limit) {
-        for (var i = pixelCount; i < 500 + pixelCount; i++) {
+        for (let i = pixelCount; i < 500 + pixelCount; i++) {
             g.fillRect(fern[i][0], fern[i][1], 1, 1);
         }
         pixelCount += 500;
